e8e8bac4766bebb1c9dbd01b62fed352f551aa14
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-16 10:02:49 -0500

    programa_2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let num1=Number (prompt("Ingrese un numero"))
+let num1 = Number(prompt("Digite el primer numero"));
 let num2 = Number(prompt("Digite el segundo numero"));
 let sum = num1 + num2;
 let rest = num1-num2;
@@ -8,3 +8,9 @@ alert("El resultado de la suma de los numeros es " +sum );
 alert ("El resultado de la resta de los numeros es " + rest);
 alert("El resultado de la multiplicacion de los numeros es " + multi);
 alert("El resultado de la division de los numeros es " + div);
+let lado1 = Number(prompt("Digite el primer lado del rectangulo"));
+let lado2 = Number(prompt("Digite el sengudo lado del rectangulo"));
+
+let area = lado1 * lado2;
+
+alert("El area del rectangulo es " + area);
