cccd393c4cc27391a85354754c01dfda8287d84a
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-16 10:14:19 -0500

    punto_9

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
-const pi = 3.14;
 let a = Number(prompt("Digite el valor de a"));
 let b = Number(prompt("Digite el valor de b"));
 let c = Number(prompt("Digite el valor de c"));
+const pi = 3.14;
+let radio = Number(prompt("Digite el radio del cilindro "));
+let altura = Number(prompt("Digite la altura del cilindro "));
 
-let radio = Number(prompt("Digite el radio de la circunferencia"));
 let resultado = (a+7*c)/(b+2-a)+2*b ;
+let area = (2 * (pi * rad**2)) + ((2 * pi * rad) * alt);
+let volumen = (pi * rad**2) * alt;
 
-let volumen = (4/3)*pi*radio**3;
-
-alert ("El volumen de la esfera es " + volumen);
 alert("El resultado de la expresion es  " + resultado);
+alert("El area del cilindro es " + area);
+alert("El volumen del cilindro es " + volumen)
