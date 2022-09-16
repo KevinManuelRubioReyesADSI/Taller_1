3f08796d08018573e45e8f0eaafca873308bf264
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-16 10:04:36 -0500

    punton_3

--- main.js
+++ main.js
@@ -1,16 +1,12 @@
-let num1 = Number(prompt("Digite el primer numero"));
-let num2 = Number(prompt("Digite el segundo numero"));
-let sum = num1 + num2;
-let rest = num1-num2;
-let multi = num1 * num2;
-let div = num1 / num2
-alert("El resultado de la suma de los numeros es " +sum );
-alert ("El resultado de la resta de los numeros es " + rest);
-alert("El resultado de la multiplicacion de los numeros es " + multi);
-alert("El resultado de la division de los numeros es " + div);
 let lado1 = Number(prompt("Digite el primer lado del rectangulo"));
 let lado2 = Number(prompt("Digite el sengudo lado del rectangulo"));
+let num1 = parseInt(prompt("Ingrese el primer numero"));
+let num2 = parseInt(prompt("Ingrese el segundo numero"));
+let num3 = parseInt(prompt("Ingrese el tercer numero"));
+let num4 = parseInt(prompt("Ingrese el cuarto numero"));
 
 let area = lado1 * lado2;
+let media = (num1+num2+num3+num4)/4;
 
 alert("El area del rectangulo es " + area);
+alert("La media de los cuatro numeros es " + media);
