bbec1859e3453e7f909df7adeba248d62788f0ca
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-16 10:05:34 -0500

    punto_4

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-let lado1 = Number(prompt("Digite el primer lado del rectangulo"));
-let lado2 = Number(prompt("Digite el sengudo lado del rectangulo"));
 let num1 = parseInt(prompt("Ingrese el primer numero"));
 let num2 = parseInt(prompt("Ingrese el segundo numero"));
 let num3 = parseInt(prompt("Ingrese el tercer numero"));
 let num4 = parseInt(prompt("Ingrese el cuarto numero"));
+let base = Number(prompt("Ingrese la base del triangulo : "));
+let altura = Number(prompt("Ingrese la altura del triangulo : "))
+let area 
+area = (base*altura) / 2
 
-let area = lado1 * lado2;
 let media = (num1+num2+num3+num4)/4;
 
-alert("El area del rectangulo es " + area);
 alert("La media de los cuatro numeros es " + media);
+alert("El area del triangulo es : " + area)
