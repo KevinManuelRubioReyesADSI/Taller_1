ff1a84576fba6a2e133ee3733c9aae6620ada3a3
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-16 09:59:49 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+let num1=Number (prompt("Ingrese un numero"))
+let num2 = Number(prompt("Digite el segundo numero"));
+let sum = num1 + num2;
+let rest = num1-num2;
+let multi = num1 * num2;
+let div = num1 / num2
+alert("El resultado de la suma de los numeros es " +sum );
+alert ("El resultado de la resta de los numeros es " + rest);
+alert("El resultado de la multiplicacion de los numeros es " + multi);
+alert("El resultado de la division de los numeros es " + div);
