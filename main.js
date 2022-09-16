eb26fac4225329d72edab30644270a31a5a14eae
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-16 10:11:43 -0500

    punto_7

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
-const pi = 3.14;
 let espacio = Number (prompt("Digite el espacio en km recorido de  la bala" ));
 let tiempo = Number (prompt("Digite el tiempo recorrido de la bala"));
+const pi = 3.14;
 
-let radio = Number (prompt("Ingrese el radio del circulo"));
 let velocidad = (espacio*1000) / (tiempo*60);
+let radio = Number(prompt("Digite el radio de la circunferencia"));
 
-let long = 2*pi*radio;
-
-let area = pi*radio*2;
-
-alert ("La longitud de la circuferencia es " + long);
-alert ("El area de la circuferencia es " + area);
 alert ("La velocidad recorrida del proyectil es " + velocidad + "'m/s'");
+let volumen = (4/3)*pi*radio**3;
+
+alert ("El volumen de la esfera es " + volumen);
