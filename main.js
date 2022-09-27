c5a2a2dff617ff944ab29f773841735c39ac65d4
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-27 09:56:14 -0500

    punto_13

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const x = 3;
-let a = parseInt(prompt('Ingrese el valor de a'));
-let b = parseInt(prompt('Ingrese el valor de b'));
-let c = parseInt(prompt('Ingrese el valor de c'));
-const precioMin=355;
-let volumen = (4/3) * Math.PI * a * b * c;
-let minuto = parseInt(prompt('Ingrese la cantidad de minutos hablados'));
-document.getElementById('raiz').innerHTML=raiz;
-let pagar = minuto*precioMin+((minuto*precioMin)*0.2);
-document.getElementById('pagar').innerHTML=pagar;
+let numeo1 = number(prompt('Ingree el primer numeor'));
+let numeo2 = number(prompt('Ingree el segundo numeor'));
+let valor = number(prompt('Ingrese el valor de la compra'));
+let tipMembresia = prompt('Ingrese el tipo de membresia que tiene, tipo A, tipo B o tipo C');
+if (numero1>numero2){
+    resta = numero1 - numero2
+    document.write('El resultado de la resta es' + resta)
+if (tipMembresia == "tipo A"){
+    totPago1 = (valor) - (valor*0.10)
+    alert("El valor a pagar por poseer la membresia tipo A  es de" + totPago1);
+}else if (tipMembresia == "tipo B"){
+    totPago2 = (valor) - (valor*0.15)
+    alert("El valor a pagar por poseer la membresia tipo B es de" + totPago2);
+}else{
+    alert("La operacion no es posible")
+    alert("El valor a pagar por no poseer ningun tipo de membresia es de " + valor);
+}
+}
