3c512adcb5a3c649795d553e05f78f49ca45068f
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-29 07:55:40 -0500

    punto_19

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const precioXhora = 15300;
-let nombre = prompt('Ingrese el nombre del empleado ');
-let horas = Number(prompt('Ingrese las horas trabajadas '));
-let pagoTotal= horas*precioXhora;
-alert ('El total a pagar por el empleado'+ nombre +  'es de ' + pagoTotal);
+let examen1 = Number(prompt('Digite la calificacion del primer examen'));
+let examen2 = Number(prompt('Digite la calificacion del segundo examen'));
+let examen3 = Number(prompt('Digite la calificacion del tercer examen'));
+let examen4 = Number(prompt('Digite la calificacion del cuarto examen'));
+if (examen1 <= 5 ) and (examen2 <= 5);{
+    pro = (exa1 + exa2 + exa3 + exa4) /4
+}
+if (examen3 <= 5) and (examen4 <=5);{
+    promedio = (examen1 + examen2 + examen3 + examen4) /4
+    alert('El promedio de las calificaciones obtenidas es ' + pro)
+}
