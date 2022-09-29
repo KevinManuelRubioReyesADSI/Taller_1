fb6093a2bfcaa05e94c6a797f7a2da5de1229a18
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-29 07:48:53 -0500

    punto_18

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
-let precioProducto = Number(prompt('Digite el precio del producto'));
-let iva = Number(prompt('digite el iva sin %'));
-let TotalIva = Iva/100;
-let IvaProducto = precioProducto* TotalIva;
-let pagoTotal =precioProducto + IvaProducto;
-alert ('El iva del producto es de ' + IvaProducto);
-alert ('El total a pagar del producto es de ' + pagoTotal);
-
+const precioXhora = 15300;
+let nombre = prompt('Ingrese el nombre del empleado ');
+let horas = Number(prompt('Ingrese las horas trabajadas '));
+let pagoTotal= horas*precioXhora;
+alert ('El total a pagar por el empleado'+ nombre +  'es de ' + pagoTotal);
