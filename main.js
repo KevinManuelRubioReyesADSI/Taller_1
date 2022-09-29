f6f626ace1c6dd445aceb6225365be3e850956a4
KevinManuelRubioReyesADSI <kevinsolo909@gmail.com>
2022-09-29 08:03:01 -0500

    punto_20

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
-let examen1 = Number(prompt('Digite la calificacion del primer examen'));
-let examen2 = Number(prompt('Digite la calificacion del segundo examen'));
-let examen3 = Number(prompt('Digite la calificacion del tercer examen'));
-let examen4 = Number(prompt('Digite la calificacion del cuarto examen'));
-if (examen1 <= 5 ) and (examen2 <= 5);{
-    pro = (exa1 + exa2 + exa3 + exa4) /4
-}
-if (examen3 <= 5) and (examen4 <=5);{
-    promedio = (examen1 + examen2 + examen3 + examen4) /4
-    alert('El promedio de las calificaciones obtenidas es ' + pro)
-}
+let sueldo = Number(prompt('Ingrese el sueldo del vendedor '));
+let venta1 = Number(prompt('Ingrese el valor total de la primera venta'));
+let venta2 = Number(prompt('Ingrese el valor total de la segunda venta'));
+let venta3 = Number(prompt('Ingrese el valor total de la tercera venta'));
+let comision =((venta1 + venta2 + venta3)/3)*0.10;
+let sueldoTotal = sueldo + comision;
+alert('La comision que le corresponde al vendedor es de ' + comision);
+alert('el sueldo total con la comision incluida es de ' + sueldoTotal);
